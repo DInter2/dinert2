f449da691abfd26908b93bf744d7c30ec1af156d
CefasCavalcanti <96499154+CefasCavalcanti@users.noreply.github.com>
2023-05-14 22:41:51 -0300

    image e drawer use client

--- src/Components/layout/drawer/DrawerServer.tsx
+++ src/Components/layout/drawer/DrawerServer.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import DrawerClient from './DrawerClient';
 import { Folder } from '@/app/types/Folder';
--- src/app/drive/[folderId]/components/files/filesCards.tsx
+++ src/app/drive/[folderId]/components/files/filesCards.tsx
@@ -1,5 +1,5 @@
+'use client'
 import { DateBr } from "@/app/core/dateConvereter"
-import { FileIcon } from "./fileIcon"
 import { FileDto } from "@/app/types/File"
 import Link from "next/link"
 import Image from "next/image"
