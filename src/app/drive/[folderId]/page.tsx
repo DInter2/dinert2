38939c6eec86e821283a7029a6c1146f540a18e2
CefasCavalcanti <96499154+CefasCavalcanti@users.noreply.github.com>
2023-07-02 22:46:42 -0300

    markdown table color gray

--- src/app/drive/[folderId]/page.tsx
+++ src/app/drive/[folderId]/page.tsx
@@ -12,7 +12,7 @@ async function getDataById(folderId: string): Promise<FolderDto> {
   const res = await fetch(
     `${appScriptUrl}?folderId=${folderId}`,
     // {cache: "no-cache"}
-    // {next: {revalidate: 60}}
+    {next: {revalidate: 60}}
     );
   if (!res.ok) {
     throw new Error('Failed to fetch data');
