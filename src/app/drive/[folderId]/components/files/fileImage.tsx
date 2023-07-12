a73c1a89c17346a382dc84037e6511f5b839a6c8
CefasCavalcanti <96499154+CefasCavalcanti@users.noreply.github.com>
2023-07-12 11:20:10 -0300

    ajuste imagem

--- src/app/drive/[folderId]/components/files/fileImage.tsx
+++ src/app/drive/[folderId]/components/files/fileImage.tsx
@@ -4,11 +4,23 @@ import Image from "next/image"
 
 export function FileImage({alt, src}: {src: string, alt: string}){
   return <Image
-  className="h-12 w-12 rounded-t object-cover"
-  width={20}
-  height={20}
-  alt={src}
-   src={"https://s3.amazonaws.com/my-bucket/profile.png"}
-   data-iml="79895.90000000224"
-   data-atf="false" />
+  className="max-w-full
+  rounded-md
+  group-hover/File:text-white
+  text-gray-800
+  object-none
+  object-top
+  pt-4
+  min-w-full"
+  src={src}
+  width={200}
+  height={200}
+  alt={alt}
+  onError={({ currentTarget,  }) => {
+    currentTarget.onerror = null;
+    currentTarget.src = '/images/google-drive.png';
+    currentTarget.classList.add("p-auto")
+  }}
+/>
 }
+
