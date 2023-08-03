e9ea3e489d2d87f55e4cfb96312e555129b12c8e
CefasCavalcanti <96499154+CefasCavalcanti@users.noreply.github.com>
2023-08-03 15:51:34 -0300

    LookeStudio icom implementation

--- src/app/drive/components/folder_card.tsx
+++ src/app/drive/components/folder_card.tsx
@@ -1,6 +1,7 @@
 import { FolderDto } from "@/types/Folder";
 import Link from "next/link";
 import { FcFolder } from "react-icons/fc";
+import { SiLooker } from "react-icons/si"
 
 function PowerBiIcon(){
   return (
@@ -12,9 +13,9 @@ function PowerBiIcon(){
 export const FolderCard = ({folder}: {folder: FolderDto}) => (
   <Link href={`/drive/${folder.id}`} scroll={false} className="group/Card col-span-1 rounded-lg py-6 px-2 bg-slate-300 ring-slate-600 shadow-lg hover:bg-gray-700 hover:ring-gray-800 duration-300 hover:-translate-y-1">
     <div className="flex w-full items-center justify-start space-x-3">
-      {folder.name.includes("(PowerBi)")? <PowerBiIcon/>: <FcFolder size={40}/>}
-      <h3 className="text-slate-900 group-hover/Card:text-white text-sm font-semibold">{ folder.name }</h3>
+      {folder.name.includes("(PowerBi)")? <PowerBiIcon/>: folder.name.includes("(LookerStudio)")? <SiLooker size={40} className="text-blue-600"/>: <FcFolder size={40}/>}
+      <h3 className="text-slate-900 group-hover/Card:text-white text-sm font-semibold">{ folder.name}</h3>
     </div>
-    <p className="text-slate-500 group-hover/Card:text-white text-sm">{folder.description}</p>
+    <p className="text-slate-500 group-hover/Card:text-white text-sm">{folder.description.replace('(page)', '')}</p>
   </Link>
 )
