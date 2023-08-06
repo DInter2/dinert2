b18b2a67224ca55d8cc9c5b40a058692ad6f4b8a
CefasCavalcanti <96499154+CefasCavalcanti@users.noreply.github.com>
2023-08-05 22:10:48 -0300

    Custon links implementation

--- src/app/Components/widgets/icons/index.tsx
+++ src/app/Components/widgets/icons/index.tsx
@@ -2,12 +2,40 @@ import { FaListAlt } from "react-icons/fa";
 import { IoMdListBox } from "react-icons/io";
 import { TbTableFilled } from "react-icons/tb";
 import { BsFillFileExcelFill, BsFillFileWordFill } from "react-icons/bs";
+import Link from "next/link";
 
-const IconForm = ()  => <span className=" h-8 mr-1">< FaListAlt color="purple"    size={25}/></span> ;
-const IconDoc = () => <span className=" h-8 mr-1"><IoMdListBox color="#4387FF"  size={30}/></span>;
-const IconSheet = () => <span className=" h-8 mr-1"><TbTableFilled color="green"  size={30}/></span>;
-const IconExcel = () => <span className=" h-8 mr-1"><BsFillFileExcelFill color="green"   size={30}/></span>;
-const IconWord = () => <span className=" h-8 mr-1"><BsFillFileWordFill color="#4387FF" size={30}/></span> ;
+const IconForm = ({children, href}:{ href: string, children: React.ReactNode}) => (
+  <a href={href} target="_blank" rel="noopener" className="p-2 space-x-2 my-2 col-span-1 rounded-lg bg-slate-300 ring-slate-600 flex w-full items-center justify-start">
+      <FaListAlt color="green"  size={30} className="mr-4"/>
+      {children}
+  </a>
+);
+
+const IconDoc = ({children, href}:{ href: string, children: React.ReactNode}) => (
+  <a href={href} target="_blank" rel="noopener" className="p-2 space-x-2 my-2 col-span-1 rounded-lg bg-slate-300 ring-slate-600 flex w-full items-center justify-start">
+      <IoMdListBox color="green"  size={30} className="mr-4"/>
+      {children}
+  </a>
+);
+
+const IconSheet = ({children, href}:{ href: string, children: React.ReactNode}) => (
+    <a href={href} target="_blank" rel="noopener" className="p-2 space-x-2 my-2 col-span-1 rounded-lg bg-slate-300 ring-slate-600 flex w-full items-center justify-start">
+        <TbTableFilled color="green"  size={30} className="mr-4"/>
+        {children}
+    </a>
+  );
+const IconExcel = ({children, href}:{ href: string, children: React.ReactNode}) => (
+  <a href={href} target="_blank" rel="noopener" className="p-2 space-x-2 my-2 col-span-1 rounded-lg bg-slate-300 ring-slate-600 flex w-full items-center justify-start">
+      <BsFillFileExcelFill color="green"  size={30} className="mr-4"/>
+      {children}
+  </a>
+);
+const IconWord = ({children, href}:{ href: string, children: React.ReactNode}) => (
+  <a href={href} target="_blank" rel="noopener" className="p-2 space-x-2 my-2 col-span-1 rounded-lg bg-slate-300 ring-slate-600 flex w-full items-center justify-start">
+      <BsFillFileWordFill color="green"  size={30} className="mr-4"/>
+      {children}
+  </a>
+);
 
 export {
   IconForm,
