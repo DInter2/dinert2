44bf5cd42dfef7ffa33a0b6fb11b1653b21cb510
CefasCavalcanti <96499154+CefasCavalcanti@users.noreply.github.com>
2023-08-10 17:26:47 -0300

    markdown component pt:20

--- src/app/[maneName]/[menuId]/layout.tsx
+++ src/app/[maneName]/[menuId]/layout.tsx
@@ -2,7 +2,7 @@ export default function PageLayout({ children }: {
   children: React.ReactNode,
 }) {
   return (
-    <div className="mx-auto sm:px-6 box-content">
+    <div className="mx-auto sm:px-6 pt-20 box-content">
       { children }
     </div>
   )
